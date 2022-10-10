88a3183908dd321ab61a7b96aa0e6c713cc4dfa0
cheddarcheese1238@gmail.com <cheddarcheese1238@gmail.com>
2022-10-10 14:46:10 -0700

    Forgot to change version

--- CarefreeGrimm.cs
+++ CarefreeGrimm.cs
@@ -11,7 +11,7 @@ namespace CarefreeGrimm
     {
         public CarefreeGrimm() : base("Carefree Grimm") { }
 
-        public override string GetVersion() => "1.0.0.0";
+        public override string GetVersion() => "1.0.0.1";
 
         public int CharmId;
 
